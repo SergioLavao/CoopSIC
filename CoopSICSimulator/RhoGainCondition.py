fba1a4d2d8ba2091d166725a2ca4e4a1c7832208
SergioLavao <sergiolavaoq@gmail.com>
2022-08-31 11:39:37 -0500

    Removed useless files

--- CoopSICSimulator/RhoGainCondition.py
+++ CoopSICSimulator/RhoGainCondition.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-
-
+#Rho1 = 1 , Rho2 = 1/2
 rho1 = 1
-rho2 = 1
+rho2 = 1/2
 
-rho1_product = 0
-rho2_product = 0
+rho1_product = 2.3467
+rho2_product = 5.403
 
 print( np.power( rho1_product,(rho2/rho1)) > rho2_product )
